03459fe817019627bf78aa52618c2fa6d0d512bc
Samuel Pickard <sjp@appmancer.com>
2024-12-22 12:58:14 +0000

    Added configuration to apply the policies to the new service account

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
 
-CONFIG_FILE=$1
-
-if [ -z "$CONFIG_FILE" ]; then
-  echo "Usage: ./deploy_all.sh <CONFIG_FILE>"
-  exit 1
-fi
-
+# Deploy role stack
 echo "Deploying role stack..."
-./scripts/deploy_stack.sh RoleStack templates/cognito_lambda_role.yaml "$CONFIG_FILE"
+./scripts/deploy_stack.sh RoleStack templates/cognito_lambda_role.yaml config/dev-parameters.json
 if [ $? -ne 0 ]; then
-  echo "Failed to deploy RoleStack. Aborting."
+  echo "Failed to deploy role stack. Exiting."
   exit 1
 fi
+echo "Successfully deployed role stack."
 
-# Fetch the exported Role ARN
-ROLE_EXPORT_NAME=$(jq -r '.Parameters[] | select(.ParameterKey=="ExportName") | .ParameterValue' config/dev-parameters.json)
-ROLE_NAME=$(aws cloudformation list-exports --query "Exports[?Name=='${ROLE_EXPORT_NAME}'].Value" --output text)
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/${ROLE_NAME}"
-if [ -z "$ROLE_ARN" ]; then
-  echo "Failed to fetch the Cognito Lambda Execution Role ARN. Aborting."
-  exit 1
-fi
-echo "Fetched Role ARN: $ROLE_ARN"
-
+# Deploy user pool stack
 echo "Deploying user pool stack..."
-./scripts/deploy_stack.sh UserPoolStack templates/cognito_user_pool.yaml "$CONFIG_FILE"
+./scripts/deploy_stack.sh UserPoolStack templates/cognito_user_pool.yaml config/dev-parameters.json
 if [ $? -ne 0 ]; then
-  echo "Failed to deploy UserPoolStack. Aborting."
+  echo "Failed to deploy user pool stack. Exiting."
   exit 1
 fi
+echo "Successfully deployed user pool stack."
 
+# Deploy service account stack
 echo "Deploying service account stack..."
-./scripts/deploy_stack.sh ServiceAccountStack templates/create_service_accounts.yaml "$CONFIG_FILE" "$ROLE_ARN"
+./scripts/deploy_stack.sh ServiceAccountStack templates/create_service_accounts.yaml config/dev-parameters.json
 if [ $? -ne 0 ]; then
-  echo "Failed to deploy ServiceAccountStack. Aborting."
+  echo "Failed to deploy service account stack. Exiting."
   exit 1
 fi
+echo "Successfully deployed service account stack."
+
+# Deploy additional policies for Cognito Lambda Execution Role
+echo "Deploying additional policies for Cognito Lambda Execution Role..."
+./scripts/deploy_stack.sh CognitoLambdaAdditionalPolicies templates/update_aws_policies.yaml config/dev-parameters.json
+if [ $? -ne 0 ]; then
+  echo "Failed to deploy additional policies for Cognito Lambda Execution Role. Exiting."
+  exit 1
+fi
+echo "Successfully deployed additional policies for Cognito Lambda Execution Role."
 
 echo "All stacks deployed successfully."
 
